18f03ac7ddb2252bc6cc25606c7c5c0f856ccaa1
michel <michel@yaltik.com>
2017-12-12 16:20:36 +0100

    def pull_data

--- etn-json.py
+++ etn-json.py
@@ -3,13 +3,19 @@ import requests, time
 from tkinter import *
 
 
-a = 1
-while a == 1:
-    """Import Electroneum json on nanopool"""
+
+def pull_data(a):
     url = requests.get('https://api.nanopool.org/v1/etn/balance/etnjyz4xSSYYsTb9e4NGut8djRgJABCMxNi1AnMKNPepQu2nQPAJcZU5ifUPHoN1yEX5ZwHHYRpLk8B9V5YzCKkRA5Cvxk8YU6')
     data_json = url.json()
     etn = data_json['data']
 
+
+
+
+a = 1
+while a == 1:
+    """Import Electroneum json on nanopool"""
+pull_data()
     if etn < 100:
         """Check if the solde is sup 100 and display your ETN"""
         etn_to_euro = float(etn) * 0.088418
@@ -22,10 +28,9 @@ while a == 1:
         fenetre = Tk()
         label = Label(fenetre, text="Nanopool ETN transfer to walett was done")
         label.pack()
-
         print ("Nanopool ETN transfer to walett was done")
 
-
+for etn
 fenetre = Tk()
 etn = Label(fenetre, text=etn)
 etn.pack()
